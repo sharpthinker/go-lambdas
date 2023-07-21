226d3db0194b8d15fd360076cb49ac0923862bd1
sharpthinker <142230055+sharpthinker@users.noreply.github.com>
2023-07-21 10:21:23 +0400

    Added ReadMe

--- terraform/us-east-1/lambda_function.tf
+++ terraform/us-east-1/lambda_function.tf
@@ -1,3 +1,8 @@
+variable "privKeyPass" {
+  type = string
+  description = "Password for private key encryption/decryption"
+}
+
 data "aws_lambda_function" "existing" {
   count         = fileexists("certGen.zip") ? 0 : 1
   function_name = "certGen-us-east-1"
@@ -14,6 +19,7 @@ resource "aws_lambda_function" "certGen-us-east-1" {
     variables = {
       CA_CERTIFICATE_ARN = "arn:aws:acm:us-east-1:923553073565:certificate/97ab6b6b-1409-4690-85ea-7be2c6947d92"
       CA_KEY_ARN         = "arn:aws:secretsmanager:us-east-1:923553073565:secret:ca/encryptedPrivKey-2HtGva"
+      PRIV_KEY_PASS      = var.privKeyPass
     }
   }
 
